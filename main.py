9c85e904acbc8c478d28fa24b411e3b9a082bb95
outf1l <outfil098@gmail.com>
2024-05-23 21:54:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ with open('list.csv', mode='r') as file:
     reader = csv.reader(file)
     for row in reader:
         data.append(row)
-element = data.pop(r.randint(0,26))
+element = data.pop(r.randint(1,len(data)))
 element = re.sub(r"(')|()|[\[-\]]","",str(element))
 print(element)
 time.sleep(5)
